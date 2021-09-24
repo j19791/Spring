33deb2baafbdded73f9d798b46aac00ac24df823
Jefferson <jeffrm@gmail.com>
2021-09-23 23:59:08 -0300

    4.1-ciclo de vida de uma entidade. Estados no insert da entidade

--- lojaJPA/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ lojaJPA/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -14,7 +14,7 @@ public class CadastroDeProduto {
 
 	public static void main(String[] args) {
 		
-		Categoria celulares = new Categoria("celulares");
+		Categoria celulares = new Categoria("celulares"); //estado Transient: nunca foi persistida (objeto Java puro). Em não esta gerenciando 
 		
 		Produto celular = new Produto("Xiami Redmi", "Muito legal", new BigDecimal("800") ,  celulares);
 	
@@ -28,12 +28,15 @@ public class CadastroDeProduto {
 		em.getTransaction().begin(); //vc nao esta usando servidor de app - transaction-type="RESOURCE_LOCAL", s/ controle de transacao automatico
 		
 		//Transient Property Value Exception: antes de salvar o produto, eu preciso salvar a categoria antes:
-		categoriaDao.cadastrar(celulares);
+		categoriaDao.cadastrar(celulares); //estado MANAGED: QDO FOI PERSISTIDO - JPA observando -   -  pode sincronizar c/ o BD
+		celulares.setNome("xpto"); //MANAGED: se for realizado uma alteração no atributo via set, o JPA faz update no bd
+		
+		
 		dao.cadastrar(celular);
 		
-		em.getTransaction().commit();
+		em.getTransaction().commit(); //gera o insert no bd
 		
-		em.close();
+		em.close();//DETACHED: Ja salvo no bd. nao esta mais gerenciado pela JPA
 		
 	}
 	
