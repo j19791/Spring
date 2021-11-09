f688230b43d2e0cb658208db0c05070d38dcff75
Jefferson <jeffrm@gmail.com>
2021-11-08 22:17:38 -0300

    Aula 5
    6-Utilizando chave composta

--- lojaJPA/src/main/java/br/com/alura/loja/modelo/Categoria.java
+++ lojaJPA/src/main/java/br/com/alura/loja/modelo/Categoria.java
@@ -1,5 +1,6 @@
 package br.com.alura.loja.modelo;
 
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,25 +11,24 @@ import javax.persistence.Table;
 @Table(name="categorias")
 public class Categoria {//enum é ruim (fixo no código)
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String nome;
+	//@Id
+	//@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//private Long id;
 	
-	
+	@EmbeddedId //mapeando chave composta
+	private CategoriaId id;
+
+
 	//quando é usado o merge - o jpa briga o uso de construtor padrão
 	public Categoria() {
 		super();
 	}
 	public Categoria(String nome) {
 		super();
-		this.nome = nome;
+		this.id = new CategoriaId(nome,"xpto");
 	}
 	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
+		return id.getNome();
 	}
 	
 	
