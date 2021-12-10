f353ec87ad32248893b3dbdddfcceb459b3c62e2
Jefferson <jeffrm@gmail.com>
2021-12-09 22:48:52 -0300

    2-Thymeleaf e Bootstrap
    5-Aplicando o Bootstrap

--- mudi/src/main/java/br/com/alura/mvc/mudi/controlller/HomeController.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controlller/HomeController.java
@@ -1,13 +1,31 @@
 package br.com.alura.mvc.mudi.controlller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.alura.mvc.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
 
 	@GetMapping("/home")
-	public String home() {
+	public String home(Model model) {
+		
+		Pedido pedido = new Pedido();
+		pedido.setNomeProduto("Xiaomi Redmi Note 8");
+		pedido.setUrlImagem("https://m.media-amazon.com/images/I/51kx1X-HduL._AC_SL1000_.jpg");
+		pedido.setUrlProduto("https://www.amazon.com.br/Smartphone-Xiaomi-Redmi-Note-Camera/dp/B07Z5BBG56");
+		pedido.setDescricao("descricao qq");
+		
+		List<Pedido> pedidos = Arrays.asList(pedido);
+		
+		model.addAttribute("pedidos", pedidos);
+		
+		
 		return "home";
 	}
 	
