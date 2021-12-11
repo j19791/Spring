4e390eb8aaface18dbd37b61f8dcaa54f3b45cca
Jefferson <jeffrm@gmail.com>
2021-12-10 23:57:59 -0300

    3-Integração c/ Spring Data
    6-Usando Spring Data JPA

--- mudi/src/main/java/br/com/alura/mvc/mudi/controlller/HomeController.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controlller/HomeController.java
@@ -2,10 +2,6 @@ package br.com.alura.mvc.mudi.controlller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,13 +15,13 @@ public class HomeController {//Gerenciada (criando instancias) pelo Spring
 //Diminuindo as responsabilidades - não tem conecção c/ bd
 	
 	@Autowired 
-	private PedidoRepository repository; //Spring disponibiliza uma instancia de PedidoRepository 
+	private PedidoRepository pedidoRepository; //Spring disponibiliza uma instancia de PedidoRepository 
 	
 	@GetMapping("/home")
 	public String home(Model model) {		
 
 		
-		List<Pedido> pedidos = repository.recuperaTodosOsPedidos();
+		List<Pedido> pedidos = pedidoRepository.findAll();
 		
 		model.addAttribute("pedidos", pedidos);
 		
--- mudi/src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -6,20 +6,14 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.mvc.mudi.model.Pedido;
 
 @Repository  //o spring gerencia instancias qdo um controller pedir
-public class PedidoRepository { 
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {//Long é o tipo do id do pedido 
 
-	//conectar c/ o bd
-		@PersistenceContext
-		private EntityManager entityManager; //Injetado a dependencia pelo Spring
 	
-	public List<Pedido> recuperaTodosOsPedidos(){
-		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
-		return query.getResultList();
-	}
 	
 }
