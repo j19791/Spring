63ce6c23732a638f8f1a1d34eee5ab3fe44da40d
Jefferson <jeffrm@gmail.com>
2021-12-16 00:08:09 -0300

    4-Trabalhando com formulário
    4-salvando Pedido

--- mudi/src/main/java/br/com/alura/mvc/mudi/controlller/PedidoController.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controlller/PedidoController.java
@@ -1,15 +1,33 @@
 package br.com.alura.mvc.mudi.controlller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.alura.mvc.mudi.dto.RequisicaoNovoPedido;
+import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 @RequestMapping("pedido")
 public class PedidoController {
 
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	
 	@GetMapping("formulario")
 	public String formulario() {
+		return "pedido/formulario";
+	}
+	
+	@PostMapping("novo") //parametros da requisicao vao ser atribudos ao objeto da classe dto RequisicaoNovoPedido
+	public String novo(RequisicaoNovoPedido requisicao) {
+		Pedido pedido = requisicao.toPedido();
+		
+		pedidoRepository.save(pedido);
 		return "pedido/formulario";
 	}
 	
